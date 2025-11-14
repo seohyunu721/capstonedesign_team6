f6438a4a0a6c945c8aaa8a4842dabad3ef043c0b
kimyubin <dbqls3141@gmail.com>
2025-11-15 01:05:55 +0900

    앱 실행할떄마다 사용자 데이터 초기화

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import '/core/theme/app_theme.dart';
 import '/screens/splash/splash_screen.dart';
-
+import 'package:shared_preferences/shared_preferences.dart'; 
 // --- 앱 시작점 ---
 void main() async {
   // Flutter 엔진과 위젯 바인딩을 초기화합니다.
   WidgetsFlutterBinding.ensureInitialized();
 
+  final prefs = await SharedPreferences.getInstance();
+  
+  // --- [임시 코드] 앱을 실행할 때마다 설정을 강제로 초기화 ---
+  await prefs.setBool('isSetupComplete', false);
+  // ----------------------------------------------------
+
+  final bool isSetupComplete = prefs.getBool('isSetupComplete') ?? false;
   runApp(const MyApp());
 }
 
